af5b6665bd9b4c23c3b8ba7a287820970bbae694
MarcelGehrig <marcel.gehrig@ntb.ch>
2017-05-17 15:03:23 +0200

    Essential features implemented and tested. And other stuff

--- Software/listNodesAndTopics.sh
+++ Software/listNodesAndTopics.sh
@@ -4,7 +4,7 @@ script="$(readlink -f $0)"
 script_dir="$(dirname $script)"
 
 
-echo rqt
-echo rqt_console
-echo rqt_graph
-echo
+echo rosnodes:; rosnode list; 
+echo;
+echo rostopics:; rostopic list;
+
